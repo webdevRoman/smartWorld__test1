cf003d6648bfd2f1ec14bc112558cf824346aa36
Roman <rgrabelikov@yandex.ru>
2019-11-02 19:35:01 +0700

    devided config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build')
 };
 
-const common = {
-  entry: {
-    'index': PATHS.src + '/pages/index/index.js',
-    'blog1': PATHS.src + '/pages/blog1/blog1.js'
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      chunks: ['index'],
-      template: PATHS.src + '/pages/index/index.pug'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'blog1.html',
-      chunks: ['blog1'],
-      template: PATHS.src + '/pages/blog1/blog1.pug'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: true
-        }
-      }
+const common = merge([
+  {
+    entry: {
+      'index': PATHS.src + '/pages/index/index.js',
+      'blog1': PATHS.src + '/pages/blog1/blog1.js'
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        chunks: ['index'],
+        template: PATHS.src + '/pages/index/index.pug'
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'blog1.html',
+        chunks: ['blog1'],
+        template: PATHS.src + '/pages/blog1/blog1.pug'
+      })
     ]
-  }
-};
-
-const developmentConfig = {
-  devServer: {
-    stats: 'errors-only',
-    port: 9000
-  }
-};
+  },
+  pug()
+]);
 
 module.exports = function(env) {
   if (env === 'production') {
     return common;
   }
   if (env === 'development') {
-    return Object.assign(
-      {},
+    return merge([
       common,
-      developmentConfig
-    );
+      devserver()
+    ]);
   }
 };
