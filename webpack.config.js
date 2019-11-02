b5604055ab1ca7c858fa1aa28b87993448af05ed
Roman <rgrabelikov@yandex.ru>
2019-11-02 23:47:20 +0700

    added MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge');
 const pug = require('./webpack/pug');
 const devserver = require('./webpack/devserver');
 const sass = require('./webpack/sass');
+const extractCSS = require('./webpack/css.extract');
 
 const PATHS = {
   src: path.join(__dirname, 'src'),
@@ -18,7 +19,7 @@ const common = merge([
     },
     output: {
       path: PATHS.build,
-      filename: '[name].js'
+      filename: 'js/[name].js'
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -38,7 +39,10 @@ const common = merge([
 
 module.exports = function(env) {
   if (env === 'production') {
-    return common;
+    return merge([
+      common,
+      extractCSS()
+    ]);
   }
   if (env === 'development') {
     return merge([
---
+++ webpack/css.extract.js
@@ -0,0 +1,98 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = function(paths) {
+  return {
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: './css/[name].css'
+      })
+    ],
+    module: {
+      rules: [{
+        test: /\.sass$/,
+        include: paths,
+        use: [
+          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      }, {
+        test: /\.css$/,
+        include: paths,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }]
+    }
+  };
+};
+
+
+
+
+
+
+
+
+
+
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// module.exports = {
+//   optimization: {
+//     splitChunks: {
+//       cacheGroups: {
+//         indexStyles: {
+//           name: 'index',
+//           test: /index\.sass/,
+//           chunks: 'all',
+//           enforce: true,
+//         },
+//         blog1Styles: {
+//           name: 'blog1',
+//           test: /blog1\.sass/,
+//           chunks: 'all',
+//           enforce: true,
+//         },
+//       },
+//     },
+//   },
+//   plugins: [
+//     new MiniCssExtractPlugin({
+//       filename: './css/[name].css'
+//     })
+//   ],
+//   module: {
+//     rules: [{
+//       test: /\.sass$/,
+//       // include: paths,
+//       use: [
+//         // 'style-loader',
+//         {
+//           loader: MiniCssExtractPlugin.loader,
+//           options: {
+//             publicPath: '../',
+//           }
+//         },
+//         'css-loader',
+//         'sass-loader'
+//       ]
+//     }, {
+//       test: /\.css$/,
+//       // include: paths,
+//       use: [
+//         // 'style-loader',
+//         MiniCssExtractPlugin.loader,
+//         'css-loader'
+//       ]
+//     }]
+//   }
+// };
