1676b0a41e4a6ef1682b309fc626a90545d5d1a6
Roman <rgrabelikov@yandex.ru>
2019-11-11 22:06:50 +0700

    started index page

--- src/pages/faq/faq.js
+++ src/pages/faq/faq.js
@@ -1,2 +1,10 @@
 import './faq.sass';
 import '../common/base.sass';
+
+const sidebarBtn = document.querySelector('.blog-btn');
+const sidebar = document.querySelector('.faq-sidebar');
+
+sidebarBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('faq-sidebar_active');
+  sidebarBtn.classList.toggle('blog-btn_active');
+});
