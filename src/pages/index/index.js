26d160dfca1048d41fc33b3b3156bee0b282260b
Roman <rgrabelikov@yandex.ru>
2019-11-12 21:48:41 +0700

    finished index page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,2 +1,21 @@
 import './index.sass';
 import '../common/base.sass';
+
+const slider = document.querySelector('.reviews-items');
+const sliderItem = document.querySelector('.reviews-item');
+const navButtons = document.querySelectorAll('.reviews-pagination__item');
+
+const slideWidth = sliderItem.offsetWidth + 30;
+
+for (let i = 0; i < navButtons.length; i++) {
+  navButtons[i].addEventListener('click', (e) => {
+    slider.style.transform = `translateX(-${slideWidth * i}px)`;
+    for (let j = 0; j < navButtons.length; j++) {
+      if (j === i) {
+        navButtons[j].classList.add('reviews-pagination__item_active');
+      } else {
+        navButtons[j].classList.remove('reviews-pagination__item_active');
+      }
+    }
+  });
+}
